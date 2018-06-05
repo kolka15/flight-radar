4893e8bfd643a9b96cde1eb59f6ba51c11c65c9d
Kozhushko Dmitriy <d.kozhushko@informika.ru>
2018-06-05 12:10:00 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -21,35 +21,6 @@ class Flights {
         return fetch(url, this.fetchOpts).then(response => response.json())
     }
 
-    init() {
-        this.getData().then(data => {
-            this.renderTable(data)
-        });
-        setInterval(()=>{
-            this.getData().then(data => {
-                this.renderTable(data)
-            });
-            console.log('refresh!');
-        }, 5000);
-    }
-
-    renderTable(data) {
-        let defaultNone = (item) => item ? item : '—';
-        let table = this.convertRecievedData(data).map((item, i) => {
-            return `
-                    <tr>
-                        <td>${defaultNone(item[1])}, ${defaultNone(item[2])}</td>
-                        <td>${defaultNone(item.data.speed)}</td>
-                        <td>${defaultNone(item.data.course)}°</td>
-                        <td>${defaultNone(item.data.altitude)}</td>
-                        <td>${item[11]} — ${item[12]}</td>
-                        <td>${defaultNone(item[13])}</td>
-                    </tr>
-                `
-        });
-        this.table.innerHTML = table.join('');
-    }
-
     convertRecievedData(data) {
         let newData = [];
 
@@ -72,9 +43,9 @@ class Flights {
         newData.forEach((item, i) => {
             item.data = {
                 distToAirport: getDistanceToAirport(item),
-                speed: +(item[5] / 0.5399568034557).toFixed(1),
+                speed: (item[5] / 0.5399568034557).toFixed(1),
                 course: item[3],
-                altitude: +(item[4] * 0.3048).toFixed(1)
+                altitude: (item[4] * 0.3048).toFixed(1)
             };
         });
 
@@ -83,6 +54,39 @@ class Flights {
         });
 
         return newData;
+    }
+
+    renderTable(data) {
+        let defaultNone = (item) => item ? item : '—';
+        let table = this.convertRecievedData(data).map((item, i) => {
+            return `
+                    <tr>
+                        <td>${defaultNone(item[1])}, ${defaultNone(item[2])}</td>
+                        <td>${defaultNone(item.data.speed)}</td>
+                        <td>${defaultNone(item.data.course)}°</td>
+                        <td>${defaultNone(item.data.altitude)}</td>
+                        <td>${item[11]} — ${item[12]}</td>
+                        <td>${defaultNone(item[13])}</td>
+                    </tr>
+                `
+        });
+        this.table.innerHTML = table.join('');
+    }
+
+    init() {
+
+        let init = () => {
+            this.getData().then(data => {
+                this.renderTable(data)
+            });
+        };
+
+        init();
+
+        setInterval(() => {
+            init();
+            console.log('refresh!');
+        }, 5000);
     }
 }
 
